ace6c9bba1c459b2ee2e3eb55e472602a2ee405f
Yevhen <Yevhen@Yevhens-MacBook-Pro.local>
2022-02-08 00:18:57 +0100

    add cache

--- src/app/services/app.service.ts
+++ src/app/services/app.service.ts
@@ -1,7 +1,10 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { shareReplay } from 'rxjs/operators';
 import { Owner, Repo } from '../models/models';
 
+const CACHE_SIZE = 1;
 
 @Injectable()
 export class AppService {
@@ -9,11 +12,34 @@ export class AppService {
   headers = new HttpHeaders().set("Accept", "application/vnd.github.v3+json");
   constructor(private http: HttpClient) {}
 
+  private reposCache$: Observable<Repo[]>;
+  private ownersCache$: Observable<Owner[]>;
+
   getRepos() {
+    if(!this.reposCache$) {
+      this.reposCache$ = this.fetchRepos().pipe(
+        shareReplay(CACHE_SIZE)
+      );
+    }
+
+    return this.reposCache$;
+  }
+  
+  getOwners() {
+    if(!this.ownersCache$) {
+      this.ownersCache$ = this.fetchOwners().pipe(
+        shareReplay(CACHE_SIZE)
+      );
+    }
+
+    return this.ownersCache$;
+  }
+
+  fetchRepos() {
     return this.http.get<Repo[]>(`${this.baseUrl}repositories`, {headers: this.headers});
   }
 
-  getOwners() {
+  fetchOwners() {
     return this.http.get<Owner[]>(`${this.baseUrl}users`, {headers: this.headers});
   }
 }
